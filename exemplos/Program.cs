f31de6e516628182d6ff383fab20d70a79ca5b6b
EzekLobo <eloliveira.cic@uesc.br>
2023-11-16 15:22:07 -0300

    Atividades em aula

---
+++ exemplos/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region 
+string[] nomes = {"João", "Maria", "Pedro"};
+foreach(string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+#endregion
+
