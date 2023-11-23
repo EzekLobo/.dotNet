c40cea9c09ef51e5b28fb3ce9c3627bcb1ae9e91
EzekLobo <eloliveira.cic@uesc.br>
2023-11-23 16:58:03 -0300

    Atividades feitas em aula

---
+++ Bug/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        long[] fatoriais = new long[num + 1];
+        fatoriais[0] = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            fatoriais[i] = i * fatoriais[i - 1];
+        }
+        Console.WriteLine($"O fatorial de {num} é {fatoriais[num]}");
+    }
+}
