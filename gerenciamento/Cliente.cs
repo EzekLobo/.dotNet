cc97ae9dfd1e34da0f29532ff817459e4b33d91b
EzekLobo <eloliveira.cic@uesc.br>
2023-11-30 17:04:58 -0300

    6.1

--- gerenciamento/Cliente.cs
+++ gerenciamento/Cliente.cs
@@ -1,33 +1,12 @@
-
-using System;
-
-using System;
-
-public class Cliente
+public class Cliente : Pessoa
 {
-    public string Nome { get; set; }
-    public DateTime DataNascimento { get; set; }
-    private string cpf;
-    public string CPF
-    {
-        get => cpf;
-        set => cpf = ValidarCPF(value) ? value : throw new ArgumentException("CPF inválido");
-    }
     public EstadoCivil EstadoCivil { get; set; }
     public string Profissao { get; set; }
 
     public Cliente(string nome, DateTime dataNascimento, string cpf, EstadoCivil estadoCivil, string profissao)
+        : base(nome, dataNascimento, cpf)
     {
-        Nome = nome;
-        DataNascimento = dataNascimento;
-        CPF = cpf;
         EstadoCivil = estadoCivil;
         Profissao = profissao;
     }
-
-    private bool ValidarCPF(string cpf)
-    {
-        return cpf.Length == 11 && cpf.All(char.IsDigit);
-    }
-    
 }
--- gerenciamento/Program.cs
+++ gerenciamento/Program.cs
@@ -8,11 +8,17 @@ class Program
     {
         EscritorioAdvocacia escritorio = new EscritorioAdvocacia();
 
-        escritorio.AdicionarAdvogado(new Advogado("Adailsssson", new DateTime(1985, 10, 15), "12345678901", "CNA123"));
-        escritorio.AdicionarAdvogado(new Advogado("francisca", new DateTime(1990, 5, 20), "98765432101", "CNA456"));
+        List<Advogado> advogados = new List<Advogado>
+        {
+            new Advogado("José", new DateTime(1985, 10, 15), "12345678901", "CNA123"),
+            new Advogado("Maria", new DateTime(1990, 5, 20), "98765432101", "CNA456")
+        };
 
-        escritorio.AdicionarCliente(new Cliente("João", new DateTime(1988, 3, 25), "11122233344", EstadoCivil.Solteiro, "Engenheiro"));
-        escritorio.AdicionarCliente(new Cliente("Ana", new DateTime(1995, 8, 10), "55566677788", EstadoCivil.Casado, "Médico"));
+        List<Cliente> clientes = new List<Cliente>
+        {
+            new Cliente("João", new DateTime(1988, 3, 25), "11122233344", EstadoCivil.Solteiro, "Engenheiro"),
+            new Cliente("Ana", new DateTime(1995, 8, 10), "55566677788", EstadoCivil.Casado, "Médico")
+        };
 
         Console.WriteLine("Relatório 1: Advogados com idade entre 30 e 40 anos");
         var advogadosPorIdade = escritorio.ObterAdvogadosPorIdade(30, 40);
