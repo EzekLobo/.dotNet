6aac8dd2da28e5e0a002ae2af6674a0413e9826a
EzekLobo <eloliveira.cic@uesc.br>
2023-11-30 16:46:37 -0300

    5-Complemento e otimização

--- gerenciamento/Advogado.cs
+++ gerenciamento/Advogado.cs
@@ -1,10 +1,27 @@
-public class Advogado : Pessoa
+using System;
+
+public class Advogado
 {
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    private string cpf;
+    public string CPF
+    {
+        get => cpf;
+        set => cpf = ValidarCPF(value) ? value : throw new ArgumentException("CPF inválido");
+    }
     public string CNA { get; set; }
 
     public Advogado(string nome, DateTime dataNascimento, string cpf, string cna)
-        : base(nome, dataNascimento, cpf)
     {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = cpf;
         CNA = cna;
+    }
+
+    private bool ValidarCPF(string cpf)
+    {
+        return cpf.Length == 11 && cpf.All(char.IsDigit);
     }
 }
