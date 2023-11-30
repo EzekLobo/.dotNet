8bb4734d0d31399f6f289adaccea9dd1a86b0a19
EzekLobo <eloliveira.cic@uesc.br>
2023-11-30 16:07:13 -0300

    2.1

--- gerenciamento/Program.cs
+++ gerenciamento/Program.cs
@@ -14,53 +14,8 @@ public class Program{
     }
 }
 
-public class Advogado : Pessoa
-{
-    public string OAB { get; set; }
-
-    public Advogado(string nome, DateTime dataNascimento, string cpf, string oab)
-        : base(nome, dataNascimento, cpf)
-    {
-        OAB = oab;
-    }
-}
-
-public class Cliente : Pessoa
-{
-    public string EstadoCivil { get; set; }
-    public string Profissao { get; set; }
-
-    public Cliente(string nome, DateTime dataNascimento, string cpf, string estadoCivil, string profissao)
-        : base(nome, dataNascimento, cpf)
-    {
-        EstadoCivil = estadoCivil;
-        Profissao = profissao;
-    }
-}
-
-public class EscritorioDeAdvocacia
-{
-    private List<Advogado> advogados;
-    private List<Cliente> clientes;
-
-    public EscritorioDeAdvocacia()
-    {
-        advogados = new List<Advogado>();
-        clientes = new List<Cliente>();
-    }
-
-    public void AdicionarAdvogado(Advogado advogado)
-    {
-        advogados.Add(advogado);
-    }
-public void AdicionarCliente(Cliente cliente)
-    {
-        clientes.Add(cliente);
-    }
 
 
 
 
-
-}
 }
