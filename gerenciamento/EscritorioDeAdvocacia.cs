6c6d762a99721c9d97bc60cf423f4252c66e1b56
EzekLobo <eloliveira.cic@uesc.br>
2023-11-30 17:42:11 -0300

    7 - Manipulação dos tratamentos de exceção

--- gerenciamento/EscritorioDeAdvocacia.cs
+++ gerenciamento/EscritorioDeAdvocacia.cs
@@ -2,36 +2,63 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+public class AdvogadoExistenteException : Exception
+{
+    public AdvogadoExistenteException(string message) : base(message) { }
+}
+
+public class ClienteExistenteException : Exception
+{
+    public ClienteExistenteException(string message) : base(message) { }
+}
 
 public class EscritorioAdvocacia
 {
     private List<Advogado> advogados = new List<Advogado>();
     private List<Cliente> clientes = new List<Cliente>();
 
-    public void AdicionarAdvogado(Advogado advogado)
+     public void AdicionarAdvogado(Advogado advogado)
     {
-        if (!advogados.Any(a => a.CPF == advogado.CPF) && !advogados.Any(a => a.CNA == advogado.CNA))
+        try
         {
+            if (advogados.Any(a => a.CPF == advogado.CPF) || advogados.Any(a => a.CNA == advogado.CNA))
+            {
+                throw new AdvogadoExistenteException("CPF ou CNA já existentes. Advogado não adicionado.");
+            }
+
             advogados.Add(advogado);
             Console.WriteLine($"Advogado {advogado.Nome} adicionado com sucesso.");
         }
-        else
+        catch (AdvogadoExistenteException ex)
         {
-            Console.WriteLine("CPF ou CNA já existentes. Advogado não adicionado.");
+            Console.WriteLine($"Erro ao adicionar advogado: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro desconhecido ao adicionar advogado: {ex.Message}");
         }
     }
 
     
-    public void AdicionarCliente(Cliente cliente)
+     public void AdicionarCliente(Cliente cliente)
     {
-        if (!clientes.Any(c => c.CPF == cliente.CPF))
+        try
         {
+            if (clientes.Any(c => c.CPF == cliente.CPF))
+            {
+                throw new ClienteExistenteException("CPF já existente. Cliente não adicionado.");
+            }
+
             clientes.Add(cliente);
             Console.WriteLine($"Cliente {cliente.Nome} adicionado com sucesso.");
         }
-        else
+        catch (ClienteExistenteException ex)
         {
-            Console.WriteLine("CPF já existente. Cliente não adicionado.");
+            Console.WriteLine($"Erro ao adicionar cliente: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro desconhecido ao adicionar cliente: {ex.Message}");
         }
     }
       
@@ -50,7 +77,28 @@ public class EscritorioAdvocacia
         return clientes.Where(c => c.EstadoCivil == estadoCivil);
     }
 
+    public IEnumerable<Cliente> ObterClientesOrdemAlfabetica()
+    {
+        return clientes.OrderBy(cliente => cliente.Nome);
+    }
     
+
+     public IEnumerable<Cliente> ObterClientesPorProfissao(string textoProfissao)
+    {
+        return clientes.Where(cliente => cliente.Profissao.Contains(textoProfissao, StringComparison.OrdinalIgnoreCase));
+    }
+    public IEnumerable<Pessoa> ObterAniversariantesDoMes(int mes)
+{
+    var aniversariantes = advogados
+        .Select(a => (Pessoa)a) // Converte Advogado para Pessoa
+        .Concat(clientes)
+        .Where(pessoa => pessoa.DataNascimento.Month == mes)
+        .OrderBy(pessoa => pessoa.DataNascimento.Day);
+
+    return aniversariantes;
+}
+
+
 
     private int CalcularIdade(DateTime dataNascimento)
     {
